a36be3e3be49062ff355d70207ab74986e0e20ea
Ahmed Toumi <ahmed.toumi2002@gmail.com>
2022-04-09 22:10:39 -0400

    Instance de la classe Film

---
+++ MonCine/Data/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonCine.Data
+{
+    internal class Film
+    {
+        public string Name { get; set; }
+    }
+}
